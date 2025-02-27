69c10a4be3c3630fa30def753003bb365b64651e
Pablo Jordán <pablo.jordan.dlf@gmail.com>
2025-02-27 14:28:42 +0100

    typo

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
         <div className="flex flex-col items-center text-center mr-8 ml-8">
           
           <p className="font-general text-sm uppercase md:text-[10px] text-blue-900">
-            El mayor regalo es vuestra acompañia, pero os dejamos nuestra cuenta por si queréis ayudarnos con nuestros comienzos
+            El mayor regalo es vuestra compañia, pero os dejamos nuestra cuenta por si queréis ayudarnos con nuestros comienzos
           </p>
           <p className="mt-2 mb-0 font-general text-[14px] uppercase text-blue-900 font-bold">
             ES12 2100 3078 9422 0032 9341
