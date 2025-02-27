e411e7c97884b21cac15d871d0751ccc6b2b52a5
Pablo Jordán <pablo.jordan.dlf@gmail.com>
2025-02-27 13:46:09 +0100

    correct

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,14 +3,14 @@ import AnimatedTitle from "./AnimatedTitle";
 const Contact = () => {
   return (
     <div id="contact" className="my-20 min-h-96 w-screen px-10">
-      <div className="relative rounded-lg bg-blue-400 py-24 text-blue-50 sm:overflow-hidden">
+      <div className="relative rounded-lg bg-white py-20 text-blue-50 sm:overflow-hidden">
 
-        <div className="flex flex-col items-center text-center">
-          <AnimatedTitle
-            title="El mayor regalo es vuestra acompañia, pero os dejamos nuestra cuenta por si queréis ayudarnos con nuestros comienzos"
-            className="special-font !text-3xl !leading-[1.2] md:!text-5xl md:!leading-[1]"
-          />
-          <p className="mt-2 mb-0 font-general text-[14px] uppercase">
+        <div className="flex flex-col items-center text-center mr-8 ml-8">
+          
+          <p className="font-general text-sm uppercase md:text-[10px] text-blue-900">
+            El mayor regalo es vuestra acompañia, pero os dejamos nuestra cuenta por si queréis ayudarnos con nuestros comienzos
+          </p>
+          <p className="mt-2 mb-0 font-general text-[14px] uppercase text-blue-900 font-bold">
             ES12 2100 3078 9422 0032 9341
           </p>
         </div>
